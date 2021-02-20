6dd0da36b6efa9315f727be6bf5478a6e1441bc5
Mangmasta <mangmasta@gmail.com>
2021-02-20 10:47:18 -0800

    Added check method for verifying the values are correct.

--- labs/lab-2/mult_tb.h
+++ labs/lab-2/mult_tb.h
@@ -3,20 +3,16 @@
 
 SC_MODULE(MultTb){
 
-/*
   sc_in<bool>  clk;
-  sc_in<int>  din1;
-  sc_in<int>  din2;
-  sc_out <int>  dout;
-*/
-  sc_in<bool> clk;
-  sc_signal<int> din1, din2, dout;
+  sc_out<int>  din1;
+  sc_out<int>  din2;
+  sc_in <int>  dout;
+
   void stim_gen();
   void check();
 
   SC_CTOR(MultTb){
    SC_THREAD(stim_gen);
-   SC_METHOD(check);
    sensitive << clk.pos();
   }
 
@@ -28,22 +24,19 @@ void MultTb::stim_gen(){
   din2 = 10;
   wait();
   printf("[MON]:: %0d * %0d = %0d\n", din1.read(), din2.read(), dout.read());
+  check();
   din1 = -10;
   din2 = 10;
   wait();
   printf("[MON]:: %0d * %0d = %0d\n", din1.read(), din2.read(), dout.read());
+  check();
 
   sc_stop(); 
 }
 
-
-void MultTb::check(){
-  if (dout != (din1 * din2)) {
-    cout << "Output is NOT correct" << endl;
-    sc_stop();
-  }
-  else {
-    cout << "The output is correct" << endl;
-    sc_stop(); 
-  }
+void MultTb::check() {
+  if(dout.read() != din1.read()*din2.read())
+    printf("ERROR, validation failed.\n");
+  else
+    printf("Verification successful.\n");
 }
